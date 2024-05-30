ebc6b026cda45553bf2fb85aa2a08ffd48668159
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-05-30 20:17:55 +0330

    implementing hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: ["./**/*.html"],
   theme: {
+    fontFamily: {
+      'sans': ['"Poetsen One"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
